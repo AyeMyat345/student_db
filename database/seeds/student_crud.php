68eb8cd13bc1d419c66537d464daa01cde3e3452
AyeMyat345 <ayemyatthandar345@gmail.com>
2019-11-06 10:44:23 +0630

    Add

---
+++ database/seeds/student_crud.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class student_crud extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
